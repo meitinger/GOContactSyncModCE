37d0a6d3500407d26ee75be1ff8542043f6b976f
createsoftware <createsoftware@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2010-11-04 20:37:11 +0000

    Update the setup file to include GPL license, update program icon (Stefan, can you check that the icon is right when installing?), and bump version number to 2.0.
    git-svn-id: https://googlesyncmod.svn.sourceforge.net/svnroot/googlesyncmod/trunk@5 1126ad72-2f51-4d33-8cd1-f97aedab7a97

--- GoogleContactsSync/Properties/AssemblyInfo.cs
+++ GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
