c43293bc145fd3d2e8f437675bb42b674c5b3846
floriwan <floriwan@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2011-03-17 12:59:24 +0000

    switched to version 3.3 and removed unnecessary link to Office.dll
    git-svn-id: https://googlesyncmod.svn.sourceforge.net/svnroot/googlesyncmod/trunk@108 1126ad72-2f51-4d33-8cd1-f97aedab7a97

--- GoogleContactsSync/Properties/AssemblyInfo.cs
+++ GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.2.2.*")]
-[assembly: AssemblyFileVersion("3.2.2")]
+[assembly: AssemblyVersion("3.3.0.*")]
+[assembly: AssemblyFileVersion("3.3.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
