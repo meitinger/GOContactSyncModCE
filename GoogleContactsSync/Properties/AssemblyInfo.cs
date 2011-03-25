747e9742421c2f36956759b7c73055d32af39166
floriwan <floriwan@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2011-03-25 17:17:22 +0000

    Fix: compiled for x86 CPU to better suppor 64 bit systems
    git-svn-id: https://googlesyncmod.svn.sourceforge.net/svnroot/googlesyncmod/trunk@135 1126ad72-2f51-4d33-8cd1-f97aedab7a97

--- GoogleContactsSync/Properties/AssemblyInfo.cs
+++ GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.3.2.*")]
-[assembly: AssemblyFileVersion("3.3.2")]
+[assembly: AssemblyVersion("3.3.3.*")]
+[assembly: AssemblyFileVersion("3.3.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
