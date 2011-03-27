60d35a0502812eb9373a1bdfb7183f6cf98d75dc
big-r <big-r@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2011-03-27 17:44:29 +0000

    fix: - allow only one instance of the program to prevent exception   (log-file exclusively locked from first instance)
    git-svn-id: https://googlesyncmod.svn.sourceforge.net/svnroot/googlesyncmod/trunk@147 1126ad72-2f51-4d33-8cd1-f97aedab7a97

--- GoogleContactsSync/Program.cs
+++ GoogleContactsSync/Program.cs
@@ -14,11 +14,21 @@ namespace GoContactSyncMod
         [STAThread]
         static void Main()
         {
+            //prevent more than one instance of the program
+            bool ok;
+            System.Threading.Mutex m = new System.Threading.Mutex(true, "acbbbc09-f76c-4874-aaff-4f3353a5a5a6", out ok);
+
+            if (!ok)
+            {
+                MessageBox.Show("Another instance of Go Contact Sync Mod is already running.","GO Contact Sync Mod",MessageBoxButtons.OK);
+                return;
+            }
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 			instance = new SettingsForm();
             Application.Run(instance);
+            GC.KeepAlive(m);
         }
 
 		internal static SettingsForm Instance
